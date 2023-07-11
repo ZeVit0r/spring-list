19d1cd0d18a7d1c5eb04e6775a63ac48a9a185b4
José Vitor <z3.vit07@gmail.com>
2023-07-11 15:48:49 -0300

    first commit

---
+++ src/main/java/com/myapp/myappspring/MyappspringApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.myappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappspringApplication.class, args);
+	}
+
+}
